b5bf3efcc1d363b8dc3efd67cd0e6a1faed8a6a2
zemcikerik <zemcik.erik@gmail.com>
2021-09-21 11:33:56 +0200

    Create backend project

---
+++ backend/src/main/java/dev/zemco/intelligenthome/backend/IntelligentHomeApplication.java
@@ -0,0 +1,13 @@
+package dev.zemco.intelligenthome.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentHomeApplication.class, args);
+	}
+
+}
