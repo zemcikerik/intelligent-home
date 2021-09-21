1b9e42e1562a9bbdcdef5696376028b74608040d
zemcikerik <zemcik.erik@gmail.com>
2021-09-21 11:33:56 +0200

    Create backend project

---
+++ backend/src/test/java/dev/zemco/intelligenthome/backend/IntelligentHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zemco.intelligenthome.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
