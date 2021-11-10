47aa839e04fe867b38b6656cca39bb139a74c1b5
zemcikerik <zemcik.erik@gmail.com>
2021-11-10 08:55:06 +0100

    App styling, loading indicator

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, Type } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 const materialModules: Type<any>[] = [
   MatButtonModule,
   MatIconModule,
+  MatProgressBarModule,
   MatSlideToggleModule
 ];
 
