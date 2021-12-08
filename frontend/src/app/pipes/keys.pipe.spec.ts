b57d19d01675d36f1f0fafe3286b7eccfead62e7
zemcikerik <zemcik.erik@gmail.com>
2021-12-08 19:47:21 +0100

    Add tests for app reducer, device service and keys pipe

--- frontend/src/app/pipes/keys.pipe.spec.ts
+++ frontend/src/app/pipes/keys.pipe.spec.ts
@@ -1,8 +1,22 @@
 import { KeysPipe } from './keys.pipe';
 
 describe('KeysPipe', () => {
-  it('create an instance', () => {
-    const pipe = new KeysPipe();
+  let pipe: KeysPipe;
+
+  beforeEach(() => pipe = new KeysPipe());
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return keys of passed object', () => {
+    // given
+    const object = { a: 1, b: 2, '^$%$': 5 };
+
+    // when
+    const result = pipe.transform(object);
+
+    // then
+    expect(result).toEqual(['a', 'b', '^$%$']);
   });
 });
--- frontend/src/app/services/device.service.spec.ts
+++ frontend/src/app/services/device.service.spec.ts
@@ -1,16 +1,39 @@
-import { TestBed } from '@angular/core/testing';
-
+import { MockBuilder, MockInstance, MockRender, MockReset } from 'ng-mocks';
 import { DeviceService } from './device.service';
+import { ServerConnectionService } from './server-connection.service';
+import { hot } from 'jest-marbles';
 
 describe('DeviceService', () => {
   let service: DeviceService;
+  let spyWatch: jest.Mock;
+
+  beforeEach(() => MockBuilder(DeviceService).mock(ServerConnectionService));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DeviceService);
+    const mockWatch = (path: string) => hot('a', { a: path });
+    spyWatch = MockInstance(ServerConnectionService, 'watch', jest.fn().mockImplementation(mockWatch));
+
+    service = MockRender(DeviceService).point.componentInstance;
   });
+
+  afterEach(MockReset);
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should watch device addition', () => {
+    expect(spyWatch).toHaveBeenCalledWith('/device/add');
+    expect(service.deviceAdd$).toBeObservable(hot('a', { a: '/device/add' }));
+  });
+
+  it('should watch device update', () => {
+    expect(spyWatch).toHaveBeenCalledWith('/device/update');
+    expect(service.deviceUpdate$).toBeObservable(hot('a', { a: '/device/update' }));
+  });
+
+  it('should watch device removal', () => {
+    expect(spyWatch).toHaveBeenCalledWith('/device/remove');
+    expect(service.deviceRemove$).toBeObservable(hot('a', { a: '/device/remove' }));
   });
 });
