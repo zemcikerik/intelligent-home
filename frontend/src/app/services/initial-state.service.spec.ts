00d55f90cdea3eaaed79fa68dc1c6c5566a3436b
zemcikerik <zemcik.erik@gmail.com>
2022-01-12 08:34:11 +0100

    Fix initial state service test suite

--- frontend/src/app/services/initial-state.service.spec.ts
+++ frontend/src/app/services/initial-state.service.spec.ts
@@ -1,14 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-
+import { MockBuilder, MockRender } from 'ng-mocks';
 import { InitialStateService } from './initial-state.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('InitialStateService', () => {
   let service: InitialStateService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(InitialStateService);
-  });
+  beforeEach(() => MockBuilder(InitialStateService).keep(HttpClientTestingModule));
+
+  beforeEach(() => service = MockRender(InitialStateService).point.componentInstance);
 
   it('should be created', () => {
     expect(service).toBeTruthy();
