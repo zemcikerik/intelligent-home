abeb712294d2cd9f96e8860f285500660bc54369
zemcikerik <zemcik.erik@gmail.com>
2022-01-28 12:22:13 +0100

    Refactor app init logic

--- frontend/src/app/store/actions/app.actions.ts
+++ frontend/src/app/store/actions/app.actions.ts
@@ -1,5 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 
-export const appEstablishConnection = createAction('[App] Establish Connection');
-export const appLoadingSuccess = createAction('[App] Loading Success');
-export const appLoadingFailure = createAction('[App] Loading Failure', props<{ error: string }>());
+export const appAuthSuccess = createAction('[App] Auth Success');
+export const appConnectSuccess = createAction('[App] Connect Success');
+export const appConnectFailure = createAction('[App] Connect Failure', props<{ error: string }>());
+export const appLoadSuccess = createAction('[App] Load Success');
+export const appLoadFailure = createAction('[App] Load Failure', props<{ error: string }>());
