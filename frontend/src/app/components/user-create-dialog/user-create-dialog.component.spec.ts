d4b9646c45b7e53de4003592f46ca3057e20b261
Erik Zemčík <zemcik.erik@gmail.com>
2022-02-07 11:33:34 +0100

    User entry styling, fix user entry/dialog tests

--- frontend/src/app/components/user-create-dialog/user-create-dialog.component.spec.ts
+++ frontend/src/app/components/user-create-dialog/user-create-dialog.component.spec.ts
@@ -1,11 +1,16 @@
 import { MockBuilder, MockedComponentFixture, MockRender } from 'ng-mocks';
 import { UserCreateDialogComponent } from './user-create-dialog.component';
+import { AppModule } from '../../app.module';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('UserCreateDialogComponent', () => {
   let fixture: MockedComponentFixture<UserCreateDialogComponent>;
   let component: UserCreateDialogComponent;
 
-  beforeEach(() => MockBuilder(UserCreateDialogComponent));
+  beforeEach(() =>
+    MockBuilder(UserCreateDialogComponent, AppModule)
+      .mock(MatDialogRef)
+  );
 
   beforeEach(() => {
     fixture = MockRender(UserCreateDialogComponent);
--- frontend/src/app/components/user-entry/user-entry.component.spec.ts
+++ frontend/src/app/components/user-entry/user-entry.component.spec.ts
@@ -1,11 +1,12 @@
 import { MockBuilder, MockedComponentFixture, MockRender } from 'ng-mocks';
 import { UserEntryComponent } from './user-entry.component';
+import { AppModule } from '../../app.module';
 
 describe('UserEntryComponent', () => {
   let fixture: MockedComponentFixture<UserEntryComponent>;
   let component: UserEntryComponent;
 
-  beforeEach(() => MockBuilder(UserEntryComponent));
+  beforeEach(() => MockBuilder(UserEntryComponent, AppModule));
 
   beforeEach(() => {
     fixture = MockRender(UserEntryComponent);
