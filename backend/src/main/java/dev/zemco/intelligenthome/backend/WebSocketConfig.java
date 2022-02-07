f0306a786e7dfbfda0bad5773719389a8cc29235
Erik Zemčík <zemcik.erik@gmail.com>
2022-02-07 15:16:02 +0100

    Add authorization header handling to STOMP CONNECT frames

--- backend/src/main/java/dev/zemco/intelligenthome/backend/WebSocketConfig.java
+++ backend/src/main/java/dev/zemco/intelligenthome/backend/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package dev.zemco.intelligenthome.backend;
 
+import dev.zemco.intelligenthome.backend.auth.AuthChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final AuthChannelInterceptor authChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -19,6 +25,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(authChannelInterceptor);
     }
 
 }
---
+++ backend/src/main/java/dev/zemco/intelligenthome/backend/auth/AuthChannelInterceptor.java
@@ -0,0 +1,36 @@
+package dev.zemco.intelligenthome.backend.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtService jwtService;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (accessor != null && accessor.getCommand() == StompCommand.CONNECT) {
+            String header = accessor.getFirstNativeHeader("Authentication");
+
+            if (header != null) {
+                String token = header.substring("Bearer ".length());
+                Authentication authentication = this.jwtService.createAuthentication(token);
+                accessor.setUser(authentication);
+            }
+        }
+
+        return message;
+    }
+
+}
