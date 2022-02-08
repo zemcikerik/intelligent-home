d79e6d52576ee2a40ce89aea0a756cd1ab1665cd
Erik Zemčík <zemcik.erik@gmail.com>
2022-02-08 18:38:41 +0100

    Reuse SecureRandom instances, reseed after 5 generated refresh tokens

--- backend/src/main/java/dev/zemco/intelligenthome/backend/auth/impl/RefreshTokenGeneratorImpl.java
+++ backend/src/main/java/dev/zemco/intelligenthome/backend/auth/impl/RefreshTokenGeneratorImpl.java
@@ -5,20 +5,26 @@ import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class RefreshTokenGeneratorImpl implements RefreshTokenGenerator {
 
+    private final SecureRandom rng = new SecureRandom();
+    private final AtomicInteger generationCount = new AtomicInteger();
+
     @Override
     public String generateRefreshToken() {
-        // TODO: reuse SecureRandom
-        // TODO: reseeding
-        SecureRandom rng = new SecureRandom();
-
         // we need 32 characters
         // https://stackoverflow.com/a/17864767
         byte[] bytes = new byte[24];
-        rng.nextBytes(bytes);
+        this.rng.nextBytes(bytes);
+
+        // reseed after 5 generated tokens
+        if (this.generationCount.incrementAndGet() == 5) {
+            this.rng.reseed();
+            this.generationCount.set(0);
+        }
 
         return Base64.getEncoder().encodeToString(bytes);
     }
