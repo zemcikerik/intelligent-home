ab7fdf338ea287857fc3c94bb53c9943f1a80000
juztmark <miho.marek3@gmail.com>
2022-02-19 16:35:33 +0100

    Mock devices

--- backend/src/main/java/dev/zemco/intelligenthome/backend/MockRunner.java
+++ backend/src/main/java/dev/zemco/intelligenthome/backend/MockRunner.java
@@ -5,19 +5,16 @@ import dev.zemco.intelligenthome.backend.device.DeviceService;
 import dev.zemco.intelligenthome.backend.device.impl.MockDevice;
 import dev.zemco.intelligenthome.backend.feature.*;
 import dev.zemco.intelligenthome.backend.feature.impl.MockFeature;
-import dev.zemco.intelligenthome.backend.feature.state.FeatureState;
-import dev.zemco.intelligenthome.backend.feature.state.FeatureStateFactory;
-import dev.zemco.intelligenthome.backend.feature.state.StringFeatureState;
-import dev.zemco.intelligenthome.backend.feature.state.TextFeatureState;
+import dev.zemco.intelligenthome.backend.feature.state.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Text;
 
 import javax.transaction.Transactional;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 
 @Component
@@ -37,7 +34,29 @@ public class MockRunner implements CommandLineRunner {
     @Transactional
     public void run(String... args) {
         Device thermometer = this.createDevice("Thermometer");
-        this.temperatureFeature = this.createFeature(thermometer, "Temperature", FeatureType.TEXT);
+        this.temperatureFeature = this.createFeature(thermometer, "Living Room", FeatureType.TEXT);
+        String temperature = String.format("%.2f°C", rng.nextFloat(20, 30));
+        Consumer<TextFeatureState> initialThermometerState = state -> state.setText(temperature);
+        this.createFeature(thermometer, "Kitchen", FeatureType.TEXT, initialThermometerState);
+        this.createFeature(thermometer, "Bedroom", FeatureType.TEXT, initialThermometerState);
+        this.createFeature(thermometer, "Bathroom", FeatureType.TEXT, initialThermometerState);
+
+        Device garageDoor = this.createDevice("Garage door");
+        Consumer<TextFeatureState> initialGarageDoorState = state -> state.setText("Closed");
+        this.createFeature(garageDoor, "State", FeatureType.TEXT, initialGarageDoorState);
+        this.createFeature(garageDoor, "Open/Close", FeatureType.BUTTON);
+
+
+        Device rgb = this.createDevice("RGB Lights");
+        this.createFeature(rgb, "On/Off", FeatureType.BOOLEAN);
+        List<String> rgbChoices = List.of("Flow", "Sway", "Blink");
+        Consumer<DropdownFeatureState> rgbState = state -> state.setChoices(rgbChoices);
+        this.createFeature(rgb, "Mode", FeatureType.DROPDOWN, rgbState);
+
+        Device ac = this.createDevice("AC");
+        this.createFeature(ac, "On/Off", FeatureType.BOOLEAN);
+        Consumer<IntegerFeatureState> acState = state -> state.setValue(18);
+        this.createFeature(ac, "Temperature", FeatureType.INTEGER, acState);
 
         Device shopSign = this.createDevice("Shop Sign");
         Consumer<StringFeatureState> initialShopSignState = state -> state.setText("Sale! 50%");
