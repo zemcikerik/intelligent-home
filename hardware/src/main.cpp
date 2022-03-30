15a5e132ecbe1f92c9a2fe99d9b5a03202b1d379
Erik Zemčík <zemcik.erik@gmail.com>
2022-03-30 16:47:09 +0200

    Implement thermometer device

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -2,24 +2,26 @@
 
 #include <WiFi.h>
 #include <string>
+#include <DHT.h>
 
 #include "ih/home.hpp"
 #include "config.hpp"
 
-constexpr uint16_t network_led = 18;
-constexpr uint16_t ready_led = 19;
-constexpr uint16_t status_led = 21;
-constexpr uint16_t status_button = 22;
-constexpr uint16_t relay_pin = 32;
+constexpr uint16_t network_led = 19;
+constexpr uint16_t ready_led = 21;
+
+DHT dht{5, DHT22};
 
 ih::home_manager home_manager;
-ih::device device{ "d081dc06-284b-4378-8ce5-24e71911c60d", "Power Outlet", "Controls flow of electricity" };
+ih::device device{ "d281dc06-284b-4378-8ce5-24e71911c60d", "Thermometer", "Indicates temperature" };
 
-auto relay_feature_state = std::make_shared<ih::boolean_feature_state>(false);
-ih::feature relay_feature{ "d081dc0a-284b-4378-8ce5-24e71911c60d", device.id, "Enabled", ih::feature_type::boolean, relay_feature_state };
+auto temp_feature_state = std::make_shared<ih::text_feature_state>("Waiting for update!");
+ih::feature temp_feature{ "d081dc0a-284b-4378-1ce5-24e71911c60d", device.id, "Temperature", ih::feature_type::text, temp_feature_state };
 
-bool button_handled = false;
-void set_relay(bool enabled);
+auto hum_feature_state = std::make_shared<ih::text_feature_state>("Waiting for update!");
+ih::feature hum_feature{ "d081dc0a-284a-4378-1ce5-24e71911c60d", device.id, "Humidity", ih::feature_type::text, hum_feature_state };
+
+std::string format_value(float value, std::string suffix);
 
 void setup() {
   Serial.begin(115200);
@@ -27,33 +29,45 @@ void setup() {
 
   pinMode(network_led, OUTPUT);
   pinMode(ready_led, OUTPUT);
-  pinMode(status_led, OUTPUT);
-  pinMode(status_button, INPUT_PULLUP);
-  pinMode(relay_pin, OUTPUT);
+  dht.begin();
 
   WiFi.mode(WIFI_MODE_APSTA);
   WiFi.softAP(config::ap_ssid, config::ap_pswd);
 
   home_manager.set_device_info(device);
-  home_manager.register_feature(relay_feature);
-
-  home_manager.on_feature_update_request(relay_feature, [](ih::feature&, JsonObject& update) {
-    set_relay(update["enabled"]);
-  });
+  home_manager.register_feature(temp_feature);
+  home_manager.register_feature(hum_feature);
 
   home_manager.enable_web_server();
   home_manager.begin();
+
+  xTaskCreate([](void*) {
+    while (true) {
+      const auto temperature = dht.readTemperature();
+      const auto humidity = dht.readHumidity();
+
+      temp_feature_state->text = format_value(temperature, " °C");
+      home_manager.update_feature(temp_feature);
+
+      hum_feature_state->text = format_value(humidity, " %");
+      home_manager.update_feature(hum_feature);
+
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+  }, "Sample Task", 4096, nullptr, 2, nullptr);
+}
+
+std::string format_value(float value, std::string suffix) {
+  // workaround because of compiler bug
+  std::stringstream ss;
+  ss << value;
+  ss << suffix;
+
+  return ss.str();
 }
 
 void loop() {
   home_manager.loop();
-
-  if (!button_handled && digitalRead(status_button) == LOW) {
-    set_relay(!relay_feature_state->enabled);
-    delay(200);
-  } else if (digitalRead(status_button) == HIGH) {
-    button_handled = false;
-  }
 
   const auto state = home_manager.get_state();
 
@@ -64,11 +78,4 @@ void loop() {
     digitalWrite(network_led, LOW);
     digitalWrite(ready_led, LOW);
   }
-}
-
-void set_relay(bool enabled) {
-  digitalWrite(relay_pin, enabled ? HIGH : LOW);
-  digitalWrite(status_led, enabled ? HIGH : LOW);
-  relay_feature_state->enabled = enabled;
-  home_manager.update_feature(relay_feature);
 }
